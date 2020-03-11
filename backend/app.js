b9c1007af88a93e9eadb62e56028d86547720c7f
Cristian <schweizercristian@gmail.com>
2020-03-11 10:44:11 +0100

    remove expo push from back

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,7 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const Nexmo = require('nexmo');
-const { Expo } = require('expo-server-sdk');
 
-let expo = new Expo();
 //app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ limit: '1000mb',parameterLimit: 100000,extended: true  }));
@@ -363,8 +361,7 @@ app.post('/sms_verify', function (req, res) {
             let userId = response.dataValues.id    
             User.update(
             { 
-                verifyCode: verifyCode,
-                expoPushToken: req.body.pushToken, 
+                verifyCode: verifyCode, 
             }, 
             {
                 where: {
@@ -407,7 +404,7 @@ app.post('/sms_verify', function (req, res) {
                 digits: prefix + localCode + num,
                 verifyCode: verifyCode,
                 deviceId: deviceId,
-                expoPushToken: req.body.pushToken,
+                expoPushToken: '',
             })
             .then((response) => {
                 
@@ -561,99 +558,6 @@ app.post('/check_first_time', function (req, res) {
             res.status(200).send(false)
         }
       })
-})
-
-app.post('/not_test', authToken, function (req, res){
-    console.log(req);
-    let messages = [];
-    for (let pushToken of req.body[1]) {
-        // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
-
-        // Check that all your push tokens appear to be valid Expo push tokens
-        if (!Expo.isExpoPushToken(pushToken)) {
-            console.error(`Push token ${pushToken} is not a valid Expo push token`);
-            continue;
-        }
-
-        // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications)
-        messages.push({
-            to: pushToken,
-            sound: 'default',
-            badge: 1,
-            title: req.body[0].name_last,
-            body: req.body[0].message,
-            data: { withSome: 'data' },
-        })
-    }
-
-    // The Expo push notification service accepts batches of notifications so
-    // that you don't need to send 1000 requests to send 1000 notifications. We
-    // recommend you batch your notifications to reduce the number of requests
-    // and to compress them (notifications with similar content will get
-    // compressed).
-    let chunks = expo.chunkPushNotifications(messages);
-    let tickets = [];
-    (async () => {
-        // Send the chunks to the Expo push notification service. There are
-        // different strategies you could use. A simple one is to send one chunk at a
-        // time, which nicely spreads the load out over time:
-        for (let chunk of chunks) {
-            try {
-            let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-            console.log(ticketChunk);
-            tickets.push(...ticketChunk);
-            // NOTE: If a ticket contains an error code in ticket.details.error, you
-            // must handle it appropriately. The error codes are listed in the Expo
-            // documentation:
-            // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
-            } catch (error) {
-            console.error(error);
-            }
-        }
-    })();
-
-
-    let receiptIds = [];
-    for (let ticket of tickets) {
-    // NOTE: Not all tickets have IDs; for example, tickets for notifications
-    // that could not be enqueued will have error information and no receipt ID.
-    if (ticket.id) {
-        receiptIds.push(ticket.id);
-    }
-    }
-
-    let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
-    (async () => {
-        // Like sending notifications, there are different strategies you could use
-        // to retrieve batches of receipts from the Expo service.
-        for (let chunk of receiptIdChunks) {
-            try {
-                let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
-                console.log(receipts);
-
-                // The receipts specify whether Apple or Google successfully received the
-                // notification and information about an error, if one occurred.
-                for (const receiptId in receipts) {
-                    const { status, message, details } = receipts[receiptId];
-                    if (status === "ok") {
-                        continue;
-                    } else if (status === "error") {
-                        console.error(
-                            `There was an error sending a notification: ${message}`
-                        );
-                        if (details && details.error) {
-                            // The error codes are listed in the Expo documentation:
-                            // https://docs.expo.io/versions/latest/guides/push-notifications/#individual-errors
-                            // You must handle the errors appropriately.
-                            console.error(`The error code is ${details.error}`);
-                        }
-                    }
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    })();
 })
 
 app.post('/set_user_relations', function (req, res) {
